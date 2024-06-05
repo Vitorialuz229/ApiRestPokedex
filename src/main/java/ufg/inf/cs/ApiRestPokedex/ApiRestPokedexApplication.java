f0ab95d173e5e1d8c27e0215a9582dfcd2d447cf
TanyRM <tanielemadu@outlook.com>
2024-06-05 14:55:49 -0300

    Estrutura do spring

---
+++ src/main/java/ufg/inf/cs/ApiRestPokedex/ApiRestPokedexApplication.java
@@ -0,0 +1,13 @@
+package ufg.inf.cs.ApiRestPokedex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestPokedexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestPokedexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ufg/inf/cs/ApiRestPokedex/ApiRestPokedexApplicationTests.java
@@ -0,0 +1,13 @@
+package ufg.inf.cs.ApiRestPokedex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestPokedexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
