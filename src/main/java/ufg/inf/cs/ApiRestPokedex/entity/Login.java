24b5b403788281ba57df4e73ba05922a7fbfac70
TanyRM <tanielemadu@outlook.com>
2024-06-26 10:29:14 -0300

    Entidades Login e Treinador

---
+++ src/main/java/ufg/inf/cs/ApiRestPokedex/entity/Login.java
@@ -0,0 +1,23 @@
+package ufg.inf.cs.ApiRestPokedex.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Login {
+
+    @Id
+    private int id;
+
+    @OneToOne(mappedBy = "login")
+    private Treinador treinador;
+
+    @Column(unique=true, nullable=false)
+    private String username;
+
+    @Column(unique=true, nullable=false)
+    private String email;
+
+    @Column(nullable=false)
+    private String senha;
+
+}
---
+++ src/main/java/ufg/inf/cs/ApiRestPokedex/entity/Treinador.java
@@ -0,0 +1,27 @@
+package ufg.inf.cs.ApiRestPokedex.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+public class Treinador {
+
+    @Id
+    private int id; // Este será o mesmo ID de Login
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private Date dataNascimento;
+
+    @Column
+    private int nivel;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @MapsId // Usa o ID de login para o atributo id
+    @JoinColumn(name = "id") // Indica pra qual campo vai a chave estrangeira
+    private Login login;
+
+}
